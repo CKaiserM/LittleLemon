1708639cfe75f9fad84d1dd5bfc16ab858d4a314
Mariusz <kaiser.mariusz@gmail.com>
2024-01-14 23:24:52 +0100

    db commit on main 2

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -7,12 +7,12 @@ class Menu(models.Model):
    price = models.DecimalField(max_digits=6, decimal_places=2, db_index=True)
    inventory = models.SmallIntegerField(default=5)
 
-   #def __str__(self):
-   #   return self.name
+   def __str__(self):
+      return self.title
 
 class Booking(models.Model):
     first_name = models.CharField(max_length=255)
-    reservation_date = models.DateField()
+    booking_date = models.DateField()
     no_of_guests = models.SmallIntegerField(default=6)
 
     def __str__(self): 
