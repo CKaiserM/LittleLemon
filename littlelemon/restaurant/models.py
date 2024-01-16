7afeeed61c95c7b41f33008817079ff4dcfa65c2
Mariusz <kaiser.mariusz@gmail.com>
2024-01-16 22:46:46 +0100

    Added UT

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -6,7 +6,7 @@ class Menu(models.Model):
    title = models.CharField(max_length=255) 
    price = models.DecimalField(max_digits=6, decimal_places=2, db_index=True)
    inventory = models.SmallIntegerField(default=0)
-
+   
    def __str__(self):
         return f'{self.title} : {str(self.price)}'
 
